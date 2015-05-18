6c6d867d4ee6a53b875212de5b4e34a417a079c5
oLaudix <wow.olaudix@gmail.com>
2015-05-18 12:36:26 +0200

    asdfasdf

---
+++ external.js
@@ -0,0 +1,18 @@
+setInterval(
+
+function()
+{
+	alert("test")
+},
+3000
+);
+
+
+/*
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'script.js';
+document.head.appendChild(script);
+
+*/
---
+++ incremental.js
@@ -0,0 +1,52 @@
+var counter = 0;
+var new_tick = 0;
+var old_tick = 0;
+var adder = 1;
+//$("#testing").html(counter);
+loadData();
+function counterr()
+{
+	var start_tick = Date.now()
+	counter += adder;
+	counter = Math.round(counter * 100)/100;
+	adder = Math.floor((adder*1.01)*100)/100;
+	$("#testing").html(counter);
+	saveData();
+	var end_tick = Date.now();
+	$("#testing2").html(adder);
+	setTimeout(counterr, 1);
+};
+
+function saveData()
+{
+	//alert($("#testing").val())
+	var saveVar = 
+	{
+		attack:counter,
+		adder:adder
+	}
+	localStorage.setItem("incremental", JSON.stringify(saveVar));
+	//alert("saved")
+}
+
+function loadData()
+{
+	var loadVar = {}
+	try 
+	{
+		loadString = localStorage.getItem("incremental")
+		parsedString = JSON.parse(loadString)
+		counter = parsedString.attack
+		adder = parsedString.adder
+		//alert(counter)
+		$("#testing").html(counter)
+		$("#testing2").html(adder);
+		old_tick = Date.now()
+		setTimeout(counterr, 1);
+	}
+	catch(err)
+	{
+		
+	}
+}
+//counter();
