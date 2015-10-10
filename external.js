ed8fb2a817618605f29d3ff20e7126f1f8f379ec
oLaudix <wow.olaudix@gmail.com>
2015-10-10 17:37:10 +0200

    testerino

--- external.js
+++ external.js
@@ -40,5 +40,5 @@ var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'http://olaudix.github.io/external.js';
 document.getElementsByTagName('head')[0].appendChild(script);
-
+file:///C:/Users/oLaudix/Documents/GitHub/oLaudix.github.io/external.js
 */
