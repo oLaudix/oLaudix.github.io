ee886d224c1162ef847786152e9a34740426d1af
oLaudix <olaudix@gmail.com>
2017-04-24 20:07:34 +0200

    update
    update

--- countingshit_recipies_1.js
+++ countingshit_recipies_1.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 var items_recipies1 = {
     "science-pack-2": { name : "test", id : "science-pack-2", recipe : "science-pack-2", energy_required : 6, ingredients : [["inserter", 2], ["transport-belt", 2], ], am_type : "am6", result : 1, },
 	"inserter": { name : "", id : "inserter", recipe : "inserter", energy_required : 0.5, ingredients : [["iron-gear-wheel", 1], ["iron-plate", 1], ["basic-circuit-board", 1], ], am_type : "am6", result : 1, },
 	"transport-belt": { name : "", id : "transport-belt", recipe : "transport-belt", energy_required : 0.5, ingredients : [["iron-plate", 1], ["iron-gear-wheel", 1], ], am_type : "am6", result : 2, },
 	
 
-  
-=======
-var items_recipies1 = {
-    "science-pack-2": { name : "test", id : "science-pack-2", recipe : "science-pack-2", energy_required : 6, ingredients : [["inserter", 2], ["transport-belt", 2], ], am_type : "am6", result : 1, },
-	"inserter": { name : "", id : "inserter", recipe : "inserter", energy_required : 0.5, ingredients : [["iron-gear-wheel", 1], ["iron-plate", 1], ["basic-circuit-board", 1], ], am_type : "am6", result : 1, },
-	"transport-belt": { name : "", id : "transport-belt", recipe : "transport-belt", energy_required : 0.5, ingredients : [["iron-plate", 1], ["iron-gear-wheel", 1], ], am_type : "am6", result : 2, },
-	
-
-  
->>>>>>> origin/master
   };
