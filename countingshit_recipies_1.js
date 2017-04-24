138ace7cdc21b0360022aa3d9aed2a8330987c42
oLaudix <olaudix@gmail.com>
2017-04-24 22:30:31 +0200

    update
    update

--- countingshit_recipies_1.js
+++ countingshit_recipies_1.js
@@ -2,5 +2,5 @@ var items_recipies1 = {
     "science-pack-2": { name : "science-pack-2", id : "science-pack-2", recipe : "science-pack-2", energy_required : 6, ingredients : [["inserter", 2], ["transport-belt", 2], ], am_type : "am6", result : 1, },
 	"inserter": { name : "inserter", id : "inserter", recipe : "inserter", energy_required : 0.5, ingredients : [["iron-gear-wheel", 1], ["iron-plate", 1], ["basic-circuit-board", 1], ], am_type : "am6", result : 1, },
 	"transport-belt": { name : "transport-belt", id : "transport-belt", recipe : "transport-belt", energy_required : 0.5, ingredients : [["iron-plate", 1], ["iron-gear-wheel", 1], ], am_type : "am6", result : 2, },
-
+	"copper-ore": { name : "repl-copper-ore", id : "copper-ore", recipe : "repl-copper-ore", energy_required : 2.5, ingredients : [], am_type : "replicator5", result : 1, },
   };
